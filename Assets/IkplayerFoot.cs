105b8a0998d0655d223e00e28d057cc5aa6f87cb
IAmDirt <33249694+IAmDirt@users.noreply.github.com>
2022-10-29 14:21:06 +0200

    ik playerPrefab

---
+++ Assets/IkplayerFoot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkplayerFoot : MonoBehaviour
+{
+
+    public Transform rightFoot;
+    public Transform leftFoot;
+    public Vector3 startOffsetRight;
+    public Vector3 startOffsetLeft;
+
+    void Update()
+    {
+        updateFeet(rightFoot, startOffsetRight);
+        updateFeet(leftFoot, startOffsetLeft);
+    }
+    public Vector3 footOffset;
+    public void updateFeet(Transform foot, Vector3 offset)
+    {
+        var myOffset = transform.TransformDirection(offset);
+        var origin = transform.position + myOffset + Vector3.up * 10;
+        RaycastHit hit; // declare the RaycastHit variable
+        if (Physics.Raycast(origin, Vector3.down, out hit))
+        {
+            foot.position = hit.point + footOffset;
+        }
+    }
+}
